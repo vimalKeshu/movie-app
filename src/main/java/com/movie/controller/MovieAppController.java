f9eeb0012287a54b16f50676bb98bf0ad8546242
VIMAL CHAUDHARI <vmal.chaudhari@gmail.com>
2018-02-28 18:15:45 +0530

    Added spring security with keycloak

--- src/main/java/com/movie/controller/MovieAppController.java
+++ src/main/java/com/movie/controller/MovieAppController.java
@@ -1,7 +1,14 @@
 package com.movie.controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class MovieAppController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(MovieAppController.class);
+	
+	
 	@RequestMapping("/")
 	public String home() {
 		return "Welcome to movie app!";
 	}
+
+/*   @GetMapping("/logout")
+   public String logout(HttpServletRequest request) throws ServletException {
+      request.logout();
+      return "/";
+   }*/	
+	
+/*   @GetMapping("/sso/login")
+   public HttpServletResponse login(HttpServletRequest request,HttpServletResponse response) throws ServletException {
+      //equest.logout();
+      response.setStatus(302);
+      response.addHeader("Location", "http://localhost:8180/auth");
+      return response;
+   }	*/	
 
 }
---
+++ src/main/java/com/movie/security/SecurityConfig.java
@@ -0,0 +1,77 @@
+package com.movie.security;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter{
+	
+   /**
+    * Registers the KeycloakAuthenticationProvider with the authentication manager.
+    */
+   @Autowired
+   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+      KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+      keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+      auth.authenticationProvider(keycloakAuthenticationProvider);
+   }
+   
+   /**
+    * Sets keycloaks config resolver to use springs application.properties instead of keycloak.json (which is standard)
+    * @return
+    */
+   @Bean
+   public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+      return new KeycloakSpringBootConfigResolver();
+   }   
+   
+   /**
+    * Defines the session authentication strategy.
+    */   
+	@Bean
+	@Override
+	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+	}
+
+   @Override
+   protected void configure(HttpSecurity http) throws Exception
+   {
+      super.configure(http);
+          
+      http	
+      		.authorizeRequests() 
+/*      		.antMatchers("/movie/list/*").hasRole("corvesta-user")*/
+            .antMatchers("/movie/*").hasRole("corvesta-admin")           
+            .anyRequest().permitAll();
+   }	
+   
+   @Bean
+   public FilterRegistrationBean keycloakPreAuthActionsFilterRegistrationBean(
+           KeycloakPreAuthActionsFilter filter) {
+       FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+       registrationBean.setEnabled(false);
+       return registrationBean;
+   }   
+	
+	
+}
